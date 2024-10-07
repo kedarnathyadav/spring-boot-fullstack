fc57d86baa92e1cccefbe9bab782fcdf08427a5d
kedarnathyadav <dkny097@gmail.com>
2024-10-07 16:40:32 +0100

    updated pom.xml not to push image to docker

--- backend/src/main/java/com/kedarnath/Main.java
+++ backend/src/main/java/com/kedarnath/Main.java
@@ -10,8 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
-
 import java.util.Random;
 
 @SpringBootApplication
@@ -19,6 +17,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class,args);
 
     }
